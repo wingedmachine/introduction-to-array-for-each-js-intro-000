f653a08fa0985e1c8bd245db5d0075a0d52254de
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-14 21:30:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Add your doToElementsInArray() function here:
-
+var evens = [0, 2, 4, 6, 8, 10];
+ 
+evens.forEach((even, index, array) => {
+  console.log(`${even} is not odd!`);
+});
 
 // Add your changeCompletely() function here:
